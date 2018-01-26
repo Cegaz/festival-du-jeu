1c12e5e26c15aa5e95f11e691bf98b533f11acce
cegaz <cecilegaz@gmail.com>
2018-01-26 22:53:23 +0100

    change path for firework + add enable cookies

--- index.php
+++ index.php
@@ -8,16 +8,13 @@ session_start();
 	<title>Festival du jeu - compteur</title>
   <meta charset="utf-8" />
   <link href="style.css" rel="stylesheet"></style>
-  <link href="fireworks.css" rel="stylesheet"></style>
+  <link href="firework/fireworks.css" rel="stylesheet"></style>
 </head>
 
 <body id="body">
 
 	<h1>
 		<span id="counter">
-			<?php
-			$_COOKIE['counter'];
-		 ?>
 		</span>
 	</h1>
 
@@ -27,6 +24,6 @@ session_start();
 <script src="jquery-3.3.1.min.js"></script>
 <script src="js.cookie.js"></script>
 <script src="script.js"></script>
-<script src="firework.js"></script>
+<script src="firework/firework.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+    if (!navigator.cookieEnabled) {
+    alert("Pour que l'appli fonctionne, merci d'activer vos cookies.");
+    } else if (typeof Cookies.get('counter') === 'undefined' || Cookies.get('counter') === 'NaN' || Cookies.get('counter') === 'undefined') {
+        Cookies.set('counter', 0, {expires: 7});
+        window.location.replace("index.php");
+    } else {
+
     $(document).keypress(function(e) {
         e.preventDefault();
 
